b3f4f7578bae4c1874d69aa0878fd4204f79609b
vishal <vishu.mi.0007@gmail.com>
2021-10-23 23:50:36 +0530

    new

---
+++ factorian.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int factorial(int num)
+{
+    if (num == 1 || num == 0)
+    {
+        return 1;
+    }
+    else
+    {
+
+        return (num * factorial(num - 1));
+    }
+}
+
+int main()
+{
+    int i;
+    printf("enter your no want factorial \n");
+    scanf("%d", &i);
+    printf("your factorial of %d  is %d\n", i, factorial(i));
+    return 0;
+}
--- table.c
+++ table.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
+int table()
 
-
-void main()
+int main()
 {
   /* a stand for input 
      i continuety 
      a*i stand for multiplaction */
-  int a,i;
-  //input no. 
+  int a, i;
+  //input no.
   printf("enter here no.\n\n_");
-  scanf("%d",&a);
-  
-  printf("\n\nthis is the tables of  %d. \n\n",a);
-  
-  //output of table 
-  //using loop for this 
-  
-  for(i=1;i<11;i=i+1)
+  scanf("%d", &a);
+
+  printf("\n\nthis is the tables of  %d. \n\n", a);
+
+  //output of table
+  //using loop for this
+
+  for (i = 1; i < 11; i = i + 1)
   {
-    printf("%d * %d = %d\n", a,i,a*i );
-    
+    printf("%d * %d = %d\n", a, i, a * i);
   }
   return 0;
-  
 }
-
-
